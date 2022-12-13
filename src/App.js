0c2441d67d4f8fddffc854c968ce9c3f6f296cab
nagendrachoudhary <nagendradangi105@gmail.com>
2022-12-13 16:30:28 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
@@ -6,20 +6,20 @@ import Portfolio from './components/Portfolio';
 import Testimonials from  './components/Testimonials';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
-class App extends Component {
-  render() {
-    return (
+
+function App(props) {
+  {document.title='Nagendra Choudhary'}
+  return (
       <div className="App">
-        <Header/>
-        <About/>
-        <Resume/>
-        <Portfolio/>
-        <Testimonials/>
-        <ContactUs/>
-        <Footer/>
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <ContactUs />
+        <Footer />
       </div>
-    );
+      );
   }
-}
 
 export default App;
